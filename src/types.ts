11ea79168a57a5ded5dbae78759d4884bbe06424
ipkpjersi <33754783+ipkpjersi@users.noreply.github.com>
2026-08-25 12:36:59 -0400

    Added rating updates on import when rating is previously empty

--- src/types.ts
+++ src/types.ts
@@ -558,6 +558,7 @@ export enum ImportResponseType {
 	IMPORT_MULTI = "IMPORT_MULTI",
 	IMPORT_NOTFOUND = "IMPORT_NOTFOUND",
 	IMPORT_EXISTS = "IMPORT_EXISTS",
+	IMPORT_RATING_UPDATED = "IMPORT_RATING_UPDATED",
 }
 
 export interface ImportResponse {
